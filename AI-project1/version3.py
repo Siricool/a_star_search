8de9eeed3825f15849164fb56f2b498ca592121e
lindbergmatilda <matilda.lindberg97@hotmail.com>
2022-09-13 15:14:21 +0200

    framgång

--- AI-project1/version3.py
+++ AI-project1/version3.py
@@ -5,12 +5,17 @@ csv_file_name = open('100 nodes.csv')
 csv_reader = csv.reader(csv_file_name, delimiter=',')
 
 class  Node():
-    def __init__(self, parent = None, position = None):
+    def __init__(self, parent = None, position = None, index = None):
         self.parent = parent
         self.cost = 0  # detta är för att ta sig från parent till child, g är att ta sig från start till node
 
         self.positionx = position[0]
         self.positiony = position[1]
+        self.index = index
+        self.f = 0
+        self.g = 0
+        self.h = 0
+    
         
 def heuristic_function(node, end_node):
     h = abs(node.positionx - end_node.positionx) + abs(node.positiony - end_node.positiony)
@@ -35,35 +40,42 @@ def select_best_node(frontier_list, w, end_node):
 
     return node 
 
-def expand_graph_search(node):
+def expand_graph_search(node, nodes):
     child_list = []
     parent = node
+    maze=nodes
+    actions = {}
+    counter=0
+    for element in maze[node.index]:
+        if element != 0 and counter>1:
+            actions[counter-2]=element          #dictionary med state (dvs index) som key, och kostnad
+        counter +=1
+    print("ACTIONS: ", actions)
 
 
-
-    for index in len(actions): #dvs 100 nodes typ 
-        cost = actions[index] #detta blir childens path cost från tabellen dvs node.g 
+    for index, cost in actions.items(): #dvs 100 nodes typ 
+        cords = get_coordinates(maze, index)
+        child = Node(parent,cords, index)
+        child.cost=cost
+        child.g=parent.g+cost
+        child_list.append(child) #hämta rätt node, från data? 
         
-        if cost > 0: 
-            child = Node(node,)
-            child.g = cost
-            child_list.append(child) #hämta rätt node, från data? 
+    return child_list
 
 
-    return
 
 def a_star_search(nodes, start, end, weight):
     startcord = get_coordinates(nodes, start)
     endcord = get_coordinates(nodes,end)
 
-    start_node = Node(None, startcord) # start är index, vi behöver x och y för denna node
-    end_node = Node(None, endcord) # end är index, vi behöver x och y för denna node
+    start_node = Node(None, startcord, start) # start är index, vi behöver x och y för denna node
+    end_node = Node(None, endcord, end) # end är index, vi behöver x och y för denna node
 
-    frontier_list = []
-    reached_list = []
+    frontier_list = {}
+    reached_list = {}
 
-    frontier_list.append(start_node)
-    reached_list.append(start_node)
+    frontier_list[start] =start_node
+    reached_list[start] =start_node
 
     while len(frontier_list) > 0:
         node = select_best_node(frontier_list, weight, end_node)
@@ -71,7 +83,13 @@ def a_star_search(nodes, start, end, weight):
         if end_node == node:
             return node
 
-        child_list = expand_graph_search(node)
+        child_list = expand_graph_search(node, nodes)
+        print(child_list)
+
+        for child in child_list: #göra en dubbel for-loop? Så att man går igenom reached
+            if child.index in reached_list.keys():
+                print("CHILD EXIST IN REACHED!!")
+                
 
     return 
 
